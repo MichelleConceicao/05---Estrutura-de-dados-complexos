b113960893025f5cd41321e51198507a4817a92e
MichelleConceição <liliandlentretenimento@gmail.com>
2022-07-01 00:58:30 -0300

    Desafio 05

---
+++ script.js
@@ -0,0 +1,20 @@
+let meses = ["Abril", "Junho", "Setembro", "Fevereiro", "Novembro"];
+console.log(meses);
+
+
+let nomes = ["Ana", "Rodrigo", "Levi"];
+nomes.unshift("Laura");
+console.log(nomes);
+
+let sobrenomes = ["Silva", "Cruz", "Pereira"];
+sobrenomes.push("Bastos");
+console.log(sobrenomes);
+
+let objetos = ["Cadeira","Cama", "Sofá", "Geladeira"];
+objetos.shift();
+console.log(objetos);
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+let numbers = [7,5,6,3,8,9,2,1,4];
+numbers.sort();
+console.log(numbers);
